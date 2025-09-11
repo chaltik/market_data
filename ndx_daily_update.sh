5d6e5f73cb9054dcea6d9748057d5a96234da948
chaltik <karen.chaltikian@gmail.com>
2025-09-11 12:06:03 -0700

    run everything in a single script

--- ndx_daily_update.sh
+++ ndx_daily_update.sh
@@ -1,4 +1,5 @@
 tmpfile="$(mktemp -t ndx_today_XXXX).yaml"
+python update_ndx_history.py
 PYTHONPATH=. python gen_ndx_yaml.py --mode asof --asof "$(date +%F)" --output "$tmpfile"
 eval $(cat .env) PYTHONPATH=. python data_retrieval.py --assets_file "$tmpfile"
 rm -f "$tmpfile"
